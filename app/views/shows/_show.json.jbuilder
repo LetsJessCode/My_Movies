93fabe0074dd0c2b81b9c26e7af6e3a49beb9fda
LetsJessCode <letsjesscode@gmail.com>
2020-11-19 12:04:04 -0500

    added shows scaffold

---
+++ app/views/shows/_show.json.jbuilder
@@ -0,0 +1,2 @@
+json.extract! show, :id, :title, :genre, :time, :rating, :preview, :user_id, :created_at, :updated_at
+json.url show_url(show, format: :json)
---
+++ db/migrate/20201118194404_create_shows.rb
@@ -0,0 +1,15 @@
+class CreateShows < ActiveRecord::Migration[6.0]
+  def change
+    create_table :shows do |t|
+      t.string :title
+      t.string :genre
+      t.integer :time
+      t.string :rating
+      t.string :image
+      t.text :preview
+      t.references :user, null: false, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
